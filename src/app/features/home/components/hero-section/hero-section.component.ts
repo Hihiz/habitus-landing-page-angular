eaf675a216fceae86ffb9aebf218d265a77a057d
Hihiz <public@mail.ru>
2026-09-04 16:46:56 +0300

    Добавить hero секцию

---
+++ src/app/features/home/components/hero-section/hero-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-hero-section',
+  styleUrl: './hero-section.component.scss',
+  templateUrl: './hero-section.component.html',
+})
+export class HeroSectionComponent {}
