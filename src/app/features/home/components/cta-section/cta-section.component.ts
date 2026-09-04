0fa851d26af905b62ee84421626055e77b82dfde
Hihiz <public@mail.ru>
2026-09-04 16:54:18 +0300

    Добавить cta секцию

---
+++ src/app/features/home/components/cta-section/cta-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cta-section',
+  styleUrl: './cta-section.component.scss',
+  templateUrl: './cta-section.component.html',
+})
+export class CtaSectionComponent {}
